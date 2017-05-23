0d2b097be97559498bb39b99e51fd45e516e239a
Jason Sears <jsears4585@gmail.com>
2017-05-22 20:37:22 -0400

    Student ORM init

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,43 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-sql
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    sql
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-sql
+      DROP TABLE students
+    sql
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-sql
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    sql
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT * FROM students").flatten[0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 end
